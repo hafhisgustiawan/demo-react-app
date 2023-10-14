df2c3a42d16e039dbaa8312e63a610398b7902ac
Hafhis Gustiawan <hafhisgustiawan@gmail.com>
2023-10-15 04:36:52 +0700

    reinitialized commit

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -10,7 +10,8 @@ function PostPage() {
 
 export default PostPage;
 
-export function loader({ params }) {
-  const postId = params.id;
+export function loader(data) {
+  const params = data?.params;
+  const postId = params?.id;
   return fetch('https://jsonplaceholder.typicode.com/posts/' + postId);
 }
